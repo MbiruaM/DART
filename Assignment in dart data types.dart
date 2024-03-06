88362963412b26536e734cce842d57fb8b70a2f9
MbiruaM <mbiruamary6@gmail.com>
2024-03-06 17:38:17 +0300

    Assignment: Dart Data Types

---
+++ Assignment in dart data types.dart
@@ -0,0 +1,34 @@
+
+void main() {
+  //example of integer data type
+int age = 18;
+print('Age: $age');
+
+//example of double data type
+double weight = 50.4;
+print('weight: $weight');
+
+//example of string data type
+String name = 'Mumbi';
+print('name: $name');
+
+//example of boolean data type
+bool isTall = true;
+print('Is Tall: $isTall');
+
+//example of list data type
+List<String> hobbies = ["painting","colouring", "dancing"];
+print('hobbies :$hobbies');
+
+//example of maps data type
+Map<String, int> ages = {'Wki':25, 'Agf':27, 'Rkl':23};
+print("Ages of experiments: $ages");
+
+//example of runes data type
+String smiley = '\u{1F60A}'; // Unicode for smiling face emoji
+print('Smiley: $smiley');
+
+//example of null data type
+String? nullableString = null;
+print('Nullable String: $nullableString');
+}
---
+++ variable.dart
@@ -0,0 +1,15 @@
+void main() {
+  //variables in dart language
+  String school = 'PLP Africa Academy';
+  int total = 400;
+  double GPA = 4.0;
+  num year = 2024;
+  bool isMarried = true;
+
+  //lets print our variables
+  print(school);
+  print(total);
+print(GPA);
+print(year);
+print(isMarried);
+}
